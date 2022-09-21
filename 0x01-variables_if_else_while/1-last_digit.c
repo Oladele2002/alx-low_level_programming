d5a7da6f2ec6f5069d3e06e2dd6cdece68d9b728
Joshua <olajoshua68@gmail.com>
2022-09-21 06:19:08 -0700

    alphabet game

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	int n, lastDigit;
 
@@ -20,12 +19,12 @@ int main(void)
 	lastDigit = n % 10;
 	if (lastDigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d\n", n, lastDigit);
 	}
 	else if ((lastDigit < 6) && (lastDigit != 0))
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			       	n, lastDigit);
+		printf("Last digit of %d is %d and is %d",
+			 n, lastDigit, lastDigit);
 	}
 	else
 	{
@@ -33,4 +32,3 @@ int main(void)
 	}
 	return (0);
 }
-
